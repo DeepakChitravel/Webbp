11b67a9ac2f03e128080cecc428e97e27da47f91
Deepak <deepakchitravel@gmail.com>
2025-12-19 09:49:19 +0530

    changes in customer folder

--- src/lib/api/users.ts
+++ src/lib/api/users.ts
@@ -1,17 +1,17 @@
 import { apiUrl } from "@/config";
 import axios from "axios";
 
-const route = "/users";
 
-// Get user data (seller website info)
+const route = "/customers";
+
 export const userWithSite = async (slug: string) => {
-  // Correct PHP file + correct query format
   const url = `${apiUrl}${route}/site.php?slug=${slug}`;
 
   try {
     const response = await axios.get(url);
-    return response.data; // returns user or null
-  } catch (error: any) {
+    return response.data;
+  } catch (err) {
     return false;
   }
 };
+
